6033c3d9f2c6802265f65ad54da6ed875fbc8678
Moldytzu <m.alex123@yahoo.com>
2023-05-05 19:17:26 +0300

    doesn't work

--- bf.asm
+++ bf.asm
@@ -3,14 +3,73 @@ cpu 8086
 
 org 0x7C00
 
+TAPE_BASE equ 0x7E00
+
 boot:
     jmp 0:main ; far jump to known code segment
 main:
     mov ah, 0x0e ; routine to display character
-    mov al, '?'
-    int 0x10 ; call the bios
 
+    xor cx, cx ; initialise our program counter 
+    mov ds, cx ; initialise the data segment
+    mov dx, cx ; initialise the tape counter
+    mov si, program ; point to the program
+
+    mov cx, 0xFF
+    mov di, dx
+    add di, TAPE_BASE
+zero:
+    mov byte [di], 0
+    inc di
+    dec cx
+    loop zero
+
+    ; main loop
+.loop:
+    lodsb ; read character from program into al
+
+    cmp al, 0 ; halt if zero
+    jz halt
+
+    ; jump to operation handlers
+    cmp al, '+'
+    je .plus
+
+    cmp al, '.'
+    je .dump
+
+    jmp .continue
+
+.plus:
+    mov di, dx ; compute tapeCounter + tapeBase to get address of cell
+    add di, TAPE_BASE
+
+    ; increment cell
+    mov al, [di]
+    inc al
+    mov [di], al
+
+    jmp .continue
+
+.dump:
+    mov di, dx ; compute tapeCounter + tapeBase to get address of cell
+    add di, TAPE_BASE
+
+    ; display cell
+    mov al, [di]
+    int 0x10
+    jmp .continue
+
+.continue:
+    inc si ; increment program pointer
+    inc cx ; increment program counter
+
+    jmp .loop ; jump back
+
+halt:
     jmp $
+
+program: db '++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.', 0 ; displays an '$'
 
 times 510-($-$$) db 0 ; fill the rest of the sector with 0
 dw 0xAA55
--- buildrun.sh
+++ buildrun.sh
@@ -1,2 +1,3 @@
 nasm bf.asm -f bin -o boot.flp
-qemu-system-i386 -fda ./boot.flp
+qemu-system-i386 -fda ./boot.flp -s -S &
+gdb -q -ix "gdb_real.txt" -ex "target remote localhost:1234" -ex "b *0x7c00" -ex "c"
