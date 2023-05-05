530fdd9939b01dd8295813027269a6afa01ae05b
Moldytzu <m.alex123@yahoo.com>
2023-05-05 18:34:12 +0300

    hello question mark

--- bf.asm
+++ bf.asm
@@ -1,8 +1,16 @@
 bits 16
 cpu 8086
 
+org 0x7C00
+
 boot:
+    jmp 0:main ; far jump to known code segment
+main:
+    mov ah, 0x0e ; routine to display character
+    mov al, '?'
+    int 0x10 ; call the bios
+
     jmp $
 
 times 510-($-$$) db 0 ; fill the rest of the sector with 0
-dw 0xaa55
+dw 0xAA55
